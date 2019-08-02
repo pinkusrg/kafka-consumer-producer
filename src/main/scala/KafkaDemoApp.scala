d6fd3f0504fe9dbd71722f9793fc870ce812c039
Pinku Swargiary <42691400+pinkusrg@users.noreply.github.com>
2019-08-02 11:08:48 +0530

    Update KafkaDemoApp.scala

--- src/main/scala/KafkaDemoApp.scala
+++ src/main/scala/KafkaDemoApp.scala
@@ -3,7 +3,7 @@ object KafkaDemoApp extends App{
 
   val producer = new Producer
   producer.writeToKafka("coffee")
-  print("main ----------------------------------file")
+ 
   val consumer = new Consumer
   consumer.consumeFromKafka("coffee")
 
